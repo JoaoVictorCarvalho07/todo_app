b4fe9726c2d6112080db23bee2602945024182c0
JoaoVictorCarvalho07 <jvcarvalho018@gmail.com>
2025-10-17 11:30:37 -0300

    feat: adiciona verificação de resposta do servidor no frontend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
+  timeout: 5000,
 });
 
 api.interceptors.request.use((config) => {
@@ -11,8 +12,14 @@ api.interceptors.request.use((config) => {
 });
 
 api.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    return res;
+  },
+
   (err) => {
+    if (err.code === "ERR_NETWORK" || err.message.includes("Network Error")) {
+      alert("O servidor está indisponível. Tente novamente mais tarde.");
+    }
     if (
       err.response?.status === 401 ||
       err.response?.status === 403 ||
@@ -21,6 +28,12 @@ api.interceptors.response.use(
       localStorage.removeItem("token");
       alert("Sessão expirada. Faça login novamente.");
       window.location.href = "/";
+    } else if (err.response?.status === 500) {
+      alert("Erro no servidor. Tente novamente mais tarde.");
+    } else if (err.response?.status >= 500) {
+      alert("Erro interno no servidor. Por favor, tente mais tarde.");
+    } else if (err.response?.status >= 400 && err.response?.status < 500) {
+      alert("Ocorreu um erro ao processar sua requisição.");
     }
     return Promise.reject(err);
   }
